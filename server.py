3e08b3dfaee612d9cfa7f3409480ff242af115f6
Christopher Su <christophersu9@gmail.com>
2014-07-16 21:08:50 -0700

    endpoint flexibility

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ def index():
         'usage': 'GET /cal/<filter_id>/<date_field_name>/<show_version>'
     })
 
+@app.route('/cal/<filter_id>/<date_field_name>/<show_version>/', methods=['GET'])
 @app.route('/cal/<filter_id>/<date_field_name>/<show_version>', methods=['GET'])
 def get_calendar_secure(filter_id, date_field_name, show_version):
     if show_version not in ['false', 'False', '0', '']:
@@ -26,6 +27,7 @@ def get_calendar_secure(filter_id, date_field_name, show_version):
         show_version = 'false'
     return urllib2.urlopen(JIRA_BASE_URL + "/plugins/servlet/calendar?searchRequestId=" + filter_id + "&dateFieldName=" + date_field_name + "&showVersions=" + show_version + "&os_username=" + JIRA_USERNAME + "&os_password=" + JIRA_PASSWORD).read()
 
+@app.route('/cal/<filter_id>/<date_field_name>/', methods=['GET'])
 @app.route('/cal/<filter_id>/<date_field_name>', methods=['GET'])
 def get_calendar_secure_default(filter_id, date_field_name):
     return get_calendar_secure(filter_id, date_field_name, 'true')
